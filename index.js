5209e348563eec453ab2b92d258de9b56f661c63
MartinSantoswq <213374.ids.upchiapas.edu.mx>
2024-06-05 15:05:35 -0600

    last commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-  res.send('Hello, World! miss x3');
+  res.send('Hello, World! miss x3\10000000000000000-00000');
 });
 
 app.listen(port, () => {
